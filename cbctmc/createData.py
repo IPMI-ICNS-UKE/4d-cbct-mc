0d4814f045e6ef9a663ea8c72473d8fb02c8bf85
Clemens Rohling <clemens.rohling@t-online.de>
2023-01-20 16:56:26 +0100

    debugged createData

--- cbctmc/createData.py
+++ cbctmc/createData.py
@@ -8,14 +8,14 @@ def run():
         for j in range(2):
             j = j*5
             seed = np.random.randint(10, 123455463)
-            os.system("run_mc --path_ct_in /home/crohling/data/ct --filename_ct_in bin_0{}.nii".format(j) +
+            os.system("run_mc --path_ct_in /home/crohling/data/ct --filename_ct_in bin_0{}.nii ".format(j) +
                       "--path_out /home/crohling/data/results/low_{}".format("bin_0" + str(j) + "_" + str(i)) +
                       " --filename mc --photons 5e7 --gpu_id 3 "
                       "--random_seed {} --normalize False --combine_photons False".format(seed))
     for j in range(2):
         j = j * 5
         seed = np.random.randint(10, 123455463)
-        os.system("run_mc --path_ct_in /home/crohling/data/ct --filename_ct_in bin_0{}.nii".format(j) +
+        os.system("run_mc --path_ct_in /home/crohling/data/ct --filename_ct_in bin_0{}.nii ".format(j) +
                   "--path_out /home/crohling/data/results/high_{} --filename mc ".format("bin_0" + str(j)) +
                   "--photons 2.4e9 --gpu_id 3 "
                   "--random_seed {} --normalize False".format(seed))
