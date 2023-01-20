5c6b8b7fbd368b82f4880712259d7968a1e80825
Clemens Rohling <clemens.rohling@t-online.de>
2023-01-20 16:50:51 +0100

    changed number of simualtions

--- createData.py
+++ createData.py
@@ -4,20 +4,20 @@ import numpy as np
 
 
 def run():
-    for i in range(10):
+    for i in range(240):
         for j in range(2):
             j = j*5
             seed = np.random.randint(10, 123455463)
             os.system("run_mc --path_ct_in /home/crohling/data/ct --filename_ct_in bin_0{}.nii".format(j) +
                       "--path_out /home/crohling/data/results/low_{}".format("bin_0" + str(j) + "_" + str(i)) +
-                      " --filename mc --no_sim 90 --photons 5e7 --gpu_id 3 "
+                      " --filename mc --photons 5e7 --gpu_id 3 "
                       "--random_seed {} --normalize False --combine_photons False".format(seed))
     for j in range(2):
         j = j * 5
         seed = np.random.randint(10, 123455463)
         os.system("run_mc --path_ct_in /home/crohling/data/ct --filename_ct_in bin_0{}.nii".format(j) +
                   "--path_out /home/crohling/data/results/high_{} --filename mc ".format("bin_0" + str(j)) +
-                  "--no_sim 90 --photons 2.4e9 --gpu_id 3 "
+                  "--photons 2.4e9 --gpu_id 3 "
                   "--random_seed {} --normalize False".format(seed))
 
 
