38b96e8c00ae91f549c9abfd7b6afe94e8f26d16
Clemens Rohling <clemens.rohling@t-online.de>
2023-01-23 16:48:44 +0100

    docker doesn't like : in filename...

--- cbctmc/createData.py
+++ cbctmc/createData.py
@@ -13,18 +13,18 @@ def run():
                 seed = np.random.randint(10, 123455463)
                 os.system("run_mc --path_ct_in /home/crohling/data/ct/4d_ct_lung_uke_artifact_free/{}".format(id) +
                           "_4DCT_Lunge_amplitudebased_complete --filename_ct_in phase_0{}.nii ".format(j) +
-                          "--path_out /home/crohling/data/results/low_pat:{}".format(id) + "_phase:0" + str(j) +
-                          "_run:" + str(i) +
-                          " --filename low_pat:{}".format(id) + "_phase:0" + str(j) +
-                          "_run:" + str(i) + " --no_sim 90 --photons 5e7 --gpu_id 3 "
+                          "--path_out /home/crohling/data/results/low_pat{}".format(id) + "_phase0" + str(j) +
+                          "_run" + str(i) +
+                          " --filename low_pat{}".format(id) + "_phase0" + str(j) +
+                          "_run" + str(i) + " --no_sim 90 --photons 5e7 --gpu_id 3 "
                           "--random_seed {} --speed_up True --combine_photons False".format(seed))
         for j in range(2):
             j = j * 5
             seed = np.random.randint(10, 123455463)
             os.system("run_mc --path_ct_in /home/crohling/data/ct/4d_ct_lung_uke_artifact_free/{}".format(id) +
                       "_4DCT_Lunge_amplitudebased_complete --filename_ct_in phase_0{}.nii ".format(j) +
-                      "--path_out /home/crohling/data/results/HIGH_pat:{}".format(id) + "_phase:0" + str(j) +
-                      " --filename HIGH_pat:{}".format(id) + "_phase:0" + str(j) +
+                      "--path_out /home/crohling/data/results/HIGH_pat{}".format(id) + "_phase0" + str(j) +
+                      " --filename HIGH_pat{}".format(id) + "_phase0" + str(j) +
                       " --no_sim 90 --gpu_id 3 "
                       "--random_seed {} --speed_up True".format(seed))
 
