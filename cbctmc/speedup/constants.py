831a182fba7eba461c72ddb0b3e475f5f34cb343
Clemens Rohling <clemens.rohling@t-online.de>
2023-01-27 15:58:46 +0100

    constants for variance

---
+++ cbctmc/speedup/constants.py
@@ -0,0 +1,9 @@
+pixel_area = 0.006024
+mean_energy = 62889.36670284205
+factor_beam_hardening = 1.09
+n_photons_low = 5e7
+n_photons_high = 2.4e9
+scale_low_fit = 4.398481863563472
+scale_high_fit = 212.8556560184687
+scale_low_theo = pixel_area/(mean_energy*factor_beam_hardening)*n_photons_low
+scale_high_theo = pixel_area/(mean_energy*factor_beam_hardening)*n_photons_high
