ba4a0367772a9df32caa3e1005e0bb07380848d8
Frederic Madesta <frederic.madesta@gmail.com>
2023-02-03 16:55:39 +0100

    data for 2x speedup

---
+++ cbctmc/createData_2x_speedup.py
@@ -0,0 +1,23 @@
+import os
+import numpy as np
+
+
+def run():
+    arr = ["022", "024", "032", "033", "068", "069", "074", "078", "091", "092",
+           "104", "106", "109", "115", "116", "121", "124", "132", "142", "145", "146"]
+    for id in arr:
+        for i in range(5):
+            for j in range(2):
+                j = j*5
+                seed = np.random.randint(10, 123455463)
+                os.system("run_mc --path_ct_in /home/crohling/data/ct/4d_ct_lung_uke_artifact_free/{}".format(id) +
+                          "_4DCT_Lunge_amplitudebased_complete --filename_ct_in phase_0{}.nii ".format(j) +
+                          "--path_out /home/crohling/data/results/low_1.2e9/low_pat{}".format(id) + "_phase0" + str(j) +
+                          f"_run_{i:02d}" +
+                          " --filename low_pat{}".format(id) + "_phase0" + str(j) +
+                          f"_run_{i:02d}" + " --no_sim 90 --photons 1.2e9 --gpu_id 1 "
+                          "--random_seed {} --speed_up True --combine_photons False".format(seed))
+
+
+if __name__ == '__main__':
+    run()
