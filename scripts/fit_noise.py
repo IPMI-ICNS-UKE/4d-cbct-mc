6f9f932a48b9bddbea291687d61a0bf5fea0c819
Clemens Rohling <clemens.rohling@t-online.de>
2023-09-08 09:29:04 +0200

    random seed/ average runs

--- scripts/fit_noise.py
+++ scripts/fit_noise.py
@@ -42,6 +42,11 @@ from cbctmc.reconstruction.reconstruction import reconstruct_3d
     default=3e9,
 )
 @click.option(
+    "--number_runs",
+    type=int,
+    default=3,
+)
+@click.option(
     "--loglevel",
     type=click.Choice(["debug", "info", "warning", "error", "critical"]),
     default="debug",
@@ -52,10 +57,11 @@ def run(
         n_projections: int,
         lower_boundary: int,
         upper_boundary: int,
+        number_runs: int,
         loglevel: str
     ):
     function = lambda x: calcualteVarDeviation(n_histories=int(x), output_folder=output_folder, gpu=gpu,
-                                               n_projections=n_projections, loglevel=loglevel)
+                                               n_projections=n_projections, number_runs=number_runs, loglevel=loglevel)
     res = opt.minimize(function, x0=np.array(1.98e9), method="Nelder-Mead",
                        bounds=[(lower_boundary, upper_boundary)])
     pprint(res.x)
@@ -67,6 +73,7 @@ def calcualteVarDeviation(
         output_folder: Path,
         gpu: int,
         n_projections: int,
+        number_runs: int,
         loglevel: str
     ):
 
@@ -81,117 +88,108 @@ def calcualteVarDeviation(
         # create output folder
         output_folder.mkdir(parents=True, exist_ok=True)
 
+    mean_roi_stats = np.zeros(11)
 
-    simulation_config = {
-        "n_histories": n_histories,
-        "n_projections": n_projections,
-        "angle_between_projections": 360.0 / n_projections,
-    }
+    for i in range(number_runs):
+        simulation_config = {
+            "n_histories": n_histories,
+            "n_projections": n_projections,
+            "angle_between_projections": 360.0 / n_projections,
+            "random_seed": datetime.now().microsecond,
+        }
 
-    output_folder.mkdir(parents=True, exist_ok=True)
-    run_folder = f"run_{n_histories}"
+        output_folder.mkdir(parents=True, exist_ok=True)
+        run_folder = f"run_{n_histories}" + f"_run_{i}"
 
-    (output_folder / run_folder).mkdir(exist_ok=True)
+        (output_folder / run_folder).mkdir(exist_ok=True)
 
-    # # MC simulate Cat Phan 604
-    phantom = MCCatPhan604Geometry(shape=(464, 464, 250))
-    if not any((output_folder / run_folder).iterdir()):
-        phantom.save_material_segmentation(
-            output_folder / run_folder / "catphan_604_materials.nii.gz"
+        # # MC simulate Cat Phan 604
+        phantom = MCCatPhan604Geometry(shape=(464, 464, 250))
+        if not any((output_folder / run_folder).iterdir()):
+            phantom.save_material_segmentation(
+                output_folder / run_folder / "catphan_604_materials.nii.gz"
+            )
+            phantom.save_density_image(
+                output_folder / run_folder / "catphan_604_densities.nii.gz"
+            )
+
+            fp_geometry = create_geometry(start_angle=90, n_projections=n_projections)
+            save_geometry(fp_geometry, output_folder / run_folder / "geometry.xml")
+
+            simulation = MCSimulation(geometry=phantom, **simulation_config)
+            simulation.run_simulation(
+                output_folder / run_folder,
+                run_air_simulation=True,
+                clean=True,
+                gpu_id=gpu,
+                **MCDefaults().geometrical_corrections,
+                force_rerun=True,
+            )
+
+        # reconstruct MC simulation
+        # reconstruct_3d(
+        #     projections_filepath=output_folder
+        #     / run_folder
+        #     / "projections_total_normalized.mha",
+        #     geometry_filepath=output_folder / run_folder / "geometry.xml",
+        #     output_folder=output_folder / run_folder / "reconstructions",
+        #     output_filename="fdk3d.mha",
+        #     dimension=(464, 250, 464),
+        #     water_pre_correction=None,
+        # )
+        reconstruct_3d(
+            projections_filepath=output_folder
+            / run_folder
+            / "projections_total_normalized.mha",
+            geometry_filepath=output_folder / run_folder / "geometry.xml",
+            output_folder=output_folder / run_folder / "reconstructions",
+            output_filename="fdk3d_wpc.mha",
+            dimension=(464, 250, 464),
+            water_pre_correction=ReconDefaults.wpc_catphan604,
         )
-        phantom.save_density_image(
-            output_folder / run_folder / "catphan_604_densities.nii.gz"
+
+        materials = (
+            "air_1",
+            "air_2",
+            "pmp",
+            "ldpe",
+            "polystyrene",
+            "bone_020",
+            "acrylic",
+            "bone_050",
+            "delrin",
+            "teflon",
+            "water"
         )
 
-        fp_geometry = create_geometry(start_angle=90, n_projections=n_projections)
-        save_geometry(fp_geometry, output_folder / run_folder / "geometry.xml")
+        mc_recon = sitk.ReadImage(
+            str(output_folder / run_folder / "reconstructions" / "fdk3d_wpc.mha")
+        )
+        mc_recon = sitk.GetArrayFromImage(mc_recon)
 
-        simulation = MCSimulation(geometry=phantom, **simulation_config)
-        simulation.run_simulation(
-            output_folder / run_folder,
-            run_air_simulation=True,
-            clean=True,
-            gpu_id=gpu,
-            **MCDefaults().geometrical_corrections,
-            force_rerun=True,
+        mc_recon = np.moveaxis(mc_recon, 1, -1)
+        mc_recon = np.rot90(mc_recon, k=-1, axes=(0, 1))
+
+        mid_z_slice = phantom.image_shape[2] // 2
+        fig, ax = plt.subplots(1, 2, sharex=True, sharey=True)
+        ax[0].imshow(phantom.mus[..., mid_z_slice], clim=(0, 0.04))
+        ax[1].imshow(mc_recon[..., mid_z_slice], clim=(0, 0.04))
+        ax[1].set_title("MC fdk3d_wpc")
+
+        mc_roi_stats = MCCatPhan604Geometry.calculate_roi_statistics(
+            mc_recon, height_margin=2, radius_margin=2,
         )
 
-    # reconstruct MC simulation
-    # reconstruct_3d(
-    #     projections_filepath=output_folder
-    #     / run_folder
-    #     / "projections_total_normalized.mha",
-    #     geometry_filepath=output_folder / run_folder / "geometry.xml",
-    #     output_folder=output_folder / run_folder / "reconstructions",
-    #     output_filename="fdk3d.mha",
-    #     dimension=(464, 250, 464),
-    #     water_pre_correction=None,
-    # )
-    reconstruct_3d(
-        projections_filepath=output_folder
-        / run_folder
-        / "projections_total_normalized.mha",
-        geometry_filepath=output_folder / run_folder / "geometry.xml",
-        output_folder=output_folder / run_folder / "reconstructions",
-        output_filename="fdk3d_wpc.mha",
-        dimension=(464, 250, 464),
-        water_pre_correction=ReconDefaults.wpc_catphan604,
-    )
-
-    materials = (
-        "air_1",
-        "air_2",
-        "pmp",
-        "ldpe",
-        "polystyrene",
-        "bone_020",
-        "acrylic",
-        "bone_050",
-        "delrin",
-        "teflon",
-        "water"
-    )
-
-    mc_recon = sitk.ReadImage(
-        str(output_folder / run_folder / "reconstructions" / "fdk3d_wpc.mha")
-    )
-    mc_recon = sitk.GetArrayFromImage(mc_recon)
-
-    mc_recon = np.moveaxis(mc_recon, 1, -1)
-    mc_recon = np.rot90(mc_recon, k=-1, axes=(0, 1))
-
-    mid_z_slice = phantom.image_shape[2] // 2
-    fig, ax = plt.subplots(1, 2, sharex=True, sharey=True)
-    ax[0].imshow(phantom.mus[..., mid_z_slice], clim=(0, 0.04))
-    ax[1].imshow(mc_recon[..., mid_z_slice], clim=(0, 0.04))
-    ax[1].set_title("MC fdk3d_wpc")
-
-    mc_roi_stats = MCCatPhan604Geometry.calculate_roi_statistics(
-        mc_recon, height_margin=2, radius_margin=2,
-    )
-
-    print("MC fdk3d_wpc")
-    pprint(mc_roi_stats)
-    with open(str(output_folder / run_folder / "reconstructions"
-                        / f"mc_roi_stats_{str(n_histories)}.json"), "w") as file:
-        json.dump(mc_roi_stats, file, indent=6)
-
+        print("MC fdk3d_wpc")
+        pprint(mc_roi_stats)
+        with open(str(output_folder / run_folder / "reconstructions"
+                            / f"mc_roi_stats_{str(n_histories)}.json"), "w") as file:
+            json.dump(mc_roi_stats, file, indent=6)
+        mean_roi_stats += np.array([mc_roi_stats[material_name]["std"] for material_name in materials])
+    mean_roi_stats = mean_roi_stats / number_runs
     reference_roi_stats = REFERENCE_ROI_STATS_CATPHAN604_VARIAN
-
-    std_reference = [
-        reference_roi_stats[material_name]["std"] for material_name in materials
-    ]
-    std_mc = [
-        mc_roi_stats[material_name]["std"] for material_name in materials
-    ]
-    rel_dev = [
-        (mc_roi_stats[material_name]["std"] - reference_roi_stats[material_name]["std"])
-        /reference_roi_stats[material_name]["std"]
-        for material_name in materials
-    ]
-    fig, ax = plt.subplots()
-    ax.scatter(materials, std_reference, label="reference")
-    ax.scatter(materials, std_mc, label="mc")
+    reference_roi_stats = np.array([reference_roi_stats[material_name]["std"] for material_name in materials])
+    rel_dev = (mean_roi_stats - reference_roi_stats) / reference_roi_stats
     mean_rel_dev = np.mean(rel_dev)
     pprint(mean_rel_dev)
     return np.abs(mean_rel_dev)
