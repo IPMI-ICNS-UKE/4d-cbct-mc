da17fff35aba3f3bcf6768474bbb0f4832bf233c
Frederic Madesta <frederic.madesta@gmail.com>
2023-09-25 17:58:09 +0200

    updated tmux file

--- scripts/run_mc.sh
+++ scripts/run_mc.sh
@@ -16,19 +16,19 @@ if [ "$SESSIONEXISTS" = "" ]; then
 
   # create all windows and run script
   tmux new-window -t $SESSION:1 -n 'worker 1/gpu 0'
-  tmux send-keys -t 'worker 1' 'conda activate cbctmc' C-m "${CALL} --n-workers 5 --i-worker 1 --gpu 0" C-m
+  tmux send-keys -t 'worker 1' 'conda activate cbctmc' C-m "${CALL} --n-workers 3 --i-worker 1 --gpu 0" C-m
 
   tmux new-window -t $SESSION:2 -n 'worker 2/gpu 1'
-  tmux send-keys -t 'worker 2' 'conda activate cbctmc' C-m "${CALL} --n-workers 5 --i-worker 2 --gpu 1" C-m
+  tmux send-keys -t 'worker 2' 'conda activate cbctmc' C-m "${CALL} --n-workers 3 --i-worker 2 --gpu 1" C-m
 
-  tmux new-window -t $SESSION:3 -n 'worker 3/gpu 2'
-  tmux send-keys -t 'worker 3' 'conda activate cbctmc' C-m "${CALL} --n-workers 5 --i-worker 3 --gpu 2" C-m
+#  tmux new-window -t $SESSION:3 -n 'worker 3/gpu 2'
+#  tmux send-keys -t 'worker 3' 'conda activate cbctmc' C-m "${CALL} --n-workers 5 --i-worker 3 --gpu 2" C-m
 
   tmux new-window -t $SESSION:4 -n 'worker 4/gpu 4'
-  tmux send-keys -t 'worker 4' 'conda activate cbctmc' C-m "${CALL} --n-workers 5 --i-worker 4 --gpu 4" C-m
+  tmux send-keys -t 'worker 4' 'conda activate cbctmc' C-m "${CALL} --n-workers 3 --i-worker 3 --gpu 4" C-m
 
-  tmux new-window -t $SESSION:5 -n 'worker 5/gpu 5'
-  tmux send-keys -t 'worker 5' 'conda activate cbctmc' C-m "${CALL} --n-workers 5 --i-worker 5 --gpu 5" C-m
+#  tmux new-window -t $SESSION:5 -n 'worker 5/gpu 5'
+#  tmux send-keys -t 'worker 5' 'conda activate cbctmc' C-m "${CALL} --n-workers 5 --i-worker 5 --gpu 5" C-m
 fi
 
 tmux attach-session -t $SESSION:0
