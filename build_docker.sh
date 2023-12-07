773d39c840c46fddbd05e6cd298a2ecf1736e1ca
Frederic Madesta <frederic.madesta@gmail.com>
2023-12-07 21:47:11 +0100

    rework docker

--- build_docker.sh
+++ build_docker.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -x
+
 # build docker image
-docker build --tag mcgpu:latest .
+docker build --tag cbct-mc:latest .
 # do the advanced (CUDA) compiling
-container_id=$(docker run -it --detach mcgpu:latest)
-docker exec $container_id /bin/bash -c "cd /mcgpu && chmod +x ./compile.sh && ./compile.sh"
+container_id=$(docker run -it --detach cbct-mc:latest)
+docker exec $container_id /bin/bash -c "cd / && chmod +x ./compile.sh && ./compile.sh"
 docker stop $container_id
-docker commit $container_id mcgpu:latest
+docker commit $container_id cbct-mc:latest
