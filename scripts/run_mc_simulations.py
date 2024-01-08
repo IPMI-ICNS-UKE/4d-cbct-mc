46cc94ea76164e8cb3136c47648a839a9d77e0d6
Frederic Madesta <frederic.madesta@gmail.com>
2024-01-08 16:06:31 +0100

    chore(run-mc): remove debug code

--- scripts/run_mc_simulations.py
+++ scripts/run_mc_simulations.py
@@ -381,45 +381,45 @@ def run(
 
 
 if __name__ == "__main__":
-    # run()
+    run()
 
-    # for debugging
-    run(
-        [
-            "--data-folder",
-            "/datalake_fast/4d_ct_lung_uke_artifact_free",
-            "--output-folder",
-            "/datalake_fast/mc_output/4d",
-            "--phases",
-            "0",
-            "--gpu",
-            "0",
-            "--speedups",
-            "20.0",
-            "--regex",
-            "024.*",
-            "--segmenter-weights",
-            "/mnt/nas_io/anarchy/4d_cbct_mc/segmenter/2023-09-21T17:18:03.218908_run_39a7956b4719411f99ddf071__step_95000.pth",
-            "--segmenter-patch-overlap",
-            "0.25",
-            "--segmenter-patch-shape",
-            "496",
-            "496",
-            "32",
-            "--correspondence-model",
-            "/mnt/nas_io/anarchy/4d_cbct_mc/024_correspondence_model.pkl",
-            "--respiratory-signal",
-            "/mnt/nas_io/anarchy/4d_cbct_mc/024_respiratory_signal.pkl",
-            "--respiratory-signal-quantization",
-            "5",
-            "--reconstruct",
-            "--no-clean",
-            "--loglevel",
-            "debug",
-            "--n-projections",
-            "100",
-        ]
-    )
+    # # for debugging
+    # run(
+    #     [
+    #         "--data-folder",
+    #         "/datalake_fast/4d_ct_lung_uke_artifact_free",
+    #         "--output-folder",
+    #         "/datalake_fast/mc_output/4d",
+    #         "--phases",
+    #         "0",
+    #         "--gpu",
+    #         "0",
+    #         "--speedups",
+    #         "20.0",
+    #         "--regex",
+    #         "024.*",
+    #         "--segmenter-weights",
+    #         "/mnt/nas_io/anarchy/4d_cbct_mc/segmenter/2023-09-21T17:18:03.218908_run_39a7956b4719411f99ddf071__step_95000.pth",
+    #         "--segmenter-patch-overlap",
+    #         "0.25",
+    #         "--segmenter-patch-shape",
+    #         "496",
+    #         "496",
+    #         "32",
+    #         "--correspondence-model",
+    #         "/mnt/nas_io/anarchy/4d_cbct_mc/024_correspondence_model.pkl",
+    #         "--respiratory-signal",
+    #         "/mnt/nas_io/anarchy/4d_cbct_mc/024_respiratory_signal.pkl",
+    #         "--respiratory-signal-quantization",
+    #         "5",
+    #         "--reconstruct",
+    #         "--no-clean",
+    #         "--loglevel",
+    #         "debug",
+    #         "--n-projections",
+    #         "100",
+    #     ]
+    # )
 
 
-# --data-folder /mnt/nas_io/anarchy/4d_cbct_mc/4d_ct_lung_uke_artifact_free --output-folder /mnt/nas_io/anarchy/4d_cbct_mc/mc_output/4d --phases 0 --gpu 0 --gpu 1 --gpu 2--regex 024.* --segmenter-weights /mnt/nas_io/anarchy/4d_cbct_mc/segmenter/2023-09-21T17:18:03.218908_run_39a7956b4719411f99ddf071__step_95000.pth --segmenter-patch-overlap 0.25 --segmenter-patch-shape 496 496 32 --correspondence-model /mnt/nas_io/anarchy/4d_cbct_mc/024_correspondence_model.pkl --respiratory-signal /mnt/nas_io/anarchy/4d_cbct_mc/024_respiratory_signal.pkl --respiratory-signal-quantization 20 --reconstruct
+# --data-folder /mnt/nas_io/anarchy/4d_cbct_mc/4d_ct_lung_uke_artifact_free --output-folder /mnt/nas_io/anarchy/4d_cbct_mc/mc_output/4d --phases 0 --gpu 0 --gpu 1 --gpu 2--regex 024.* --segmenter-weights /mnt/nas_io/anarchy/4d_cbct_mc/segmenter/2023-09-21T17:18:03.218908_run_39a7956b4719411f99ddf071__step_95000.pth --segmenter-patch-overlap 0.25 --segmenter-patch-shape 496 496 32 --correspondence-model /mnt/nas_io/anarchy/4d_cbct_mc/024_correspondence_model.pkl --respiratory-signal /mnt/nas_io/anarchy/4d_cbct_mc/024_respiratory_signal.pkl --respiratory-signal-quantization 20 --respiratory-signal-scaling 2 --reconstruct
